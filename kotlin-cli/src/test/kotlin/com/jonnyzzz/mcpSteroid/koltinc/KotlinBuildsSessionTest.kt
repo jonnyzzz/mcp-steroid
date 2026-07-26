2728253ec06d9721946d9c735e0002e1cb21290d
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2026-07-26 14:44:56 +0200

    kotlin: Implement basic JVM compilation via build tools API
    Introduce 'KotlinBuildSession' class which allows compiling
     non-incrementally JVM sources using 'in-process' or Kotlin daemon
     execution strategy.

---
+++ kotlin-cli/src/test/kotlin/com/jonnyzzz/mcpSteroid/koltinc/KotlinBuildsSessionTest.kt
@@ -0,0 +1,38 @@
+package com.jonnyzzz.mcpSteroid.koltinc
+
+import kotlin.io.path.exists
+import kotlin.io.path.writeText
+import org.jetbrains.kotlin.buildtools.api.arguments.JvmCompilerArguments
+import org.junit.Assert.assertTrue
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TemporaryFolder
+
+
+class KotlinBuildsSessionTest {
+    @JvmField
+    @Rule
+    val tempFolder: TemporaryFolder = TemporaryFolder()
+
+    @Test
+    fun smokeCompile() {
+        val smokeDir = tempFolder.newFolder("smoke").toPath()
+        val outputJar = smokeDir.resolve("out.jar")
+        val source = smokeDir.resolve("source.kt")
+        source.writeText("""
+        fun main() { println("Hello") }
+        """.trimIndent())
+
+        KotlinBuildsSession().use {
+            it.compileKotlin(
+                sources = listOf(source),
+                destinationDir = outputJar,
+                executionPolicy = KotlinBuildsSession.CompilationExecutionPolicy.IN_PROCESS,
+            ) {
+                set(JvmCompilerArguments.CLASSPATH, listOf(it.defaultStdlibJar))
+            }
+
+            assertTrue(outputJar.exists())
+        }
+    }
+}
